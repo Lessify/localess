571c11c72c363185428661db78dec6537d86c320
alexcibotari <alexandru.cibotari@gmail.com>
2022-08-23 23:26:34 +0200

    fix issue with user sync

--- functions/src/users.ts
+++ functions/src/users.ts
@@ -14,8 +14,8 @@ export const onAuthUserCreate = auth.user()
 
     return userRef.set({
       email: user.email,
-      displayName: user.displayName,
-      photoURL: user.photoURL,
+      displayName: user.displayName || FieldValue.delete(),
+      photoURL: user.photoURL || FieldValue.delete(),
       disabled: user.disabled,
       createdOn: FieldValue.serverTimestamp(),
       updatedOn: FieldValue.serverTimestamp(),
@@ -61,12 +61,14 @@ export const usersSync = https.onCall(async (data, context) => {
 
   const listUsers = await authService.listUsers();
   listUsers.users.map(async (userRecord) => {
+    logger.debug('[usersSync] userRecord: ' + JSON.stringify(userRecord));
     const userRef = firestoreService.collection('users').doc(userRecord.uid);
     const userDS = await userRef.get();
 
+    logger.debug(`[usersSync] userDS: id='${userDS.id}' exist=${userDS.exists}`);
     if (userDS.exists) {
       const user = await userDS.data()!;
-
+      logger.debug('[usersSync] user: ' + JSON.stringify(user));
       if (
         userRecord.email !== user['email'] ||
         userRecord.displayName !== user['displayName'] ||
@@ -74,20 +76,24 @@ export const usersSync = https.onCall(async (data, context) => {
         userRecord.disabled !== user['disabled'] ||
         userRecord.customClaims?.['role'] !== user['role']
       ) {
+        logger.debug(`[usersSync] user: id='${userDS.id}' to be updated`);
         await userRef.set({
           email: userRecord.email,
-          displayName: userRecord.displayName,
-          photoURL: userRecord.photoURL,
+          displayName: userRecord.displayName || FieldValue.delete(),
+          photoURL: userRecord.photoURL || FieldValue.delete(),
           disabled: userRecord.disabled,
           role: userRecord.customClaims?.['role'],
           updatedOn: FieldValue.serverTimestamp(),
         }, {merge: true});
+      } else {
+        logger.debug(`[usersSync] user: id='${userDS.id}' no updates required`);
       }
     } else {
+      logger.debug(`[usersSync] user: id='${userDS.id}' to be added`);
       await userRef.set({
         email: userRecord.email,
-        displayName: userRecord.displayName,
-        photoURL: userRecord.photoURL,
+        displayName: userRecord.displayName || FieldValue.delete(),
+        photoURL: userRecord.photoURL || FieldValue.delete(),
         disabled: userRecord.disabled,
         role: userRecord.customClaims?.['role'],
         createdOn: FieldValue.serverTimestamp(),
@@ -99,7 +105,7 @@ export const usersSync = https.onCall(async (data, context) => {
 });
 
 export const onUserUpdate = firestore.document('users/{userId}')
-  .onUpdate((change, context) => {
+  .onUpdate(async (change, context) => {
     logger.info(`[User::onUpdate] id='${change.before.id}' eventId='${context.eventId}'`);
     const before = change.before.data();
     const after = change.after.data();
@@ -109,7 +115,15 @@ export const onUserUpdate = firestore.document('users/{userId}')
     const roleAfter = after['role'];
     if (roleBefore !== roleAfter) {
       logger.info(`[User::onUpdate::RoleChange] id='${change.before.id}' eventId='${context.eventId}' from='${roleBefore}' to='${roleAfter}'`);
-      return authService.setCustomUserClaims(change.before.id, {role: roleAfter});
+      const userRecord = await authService.getUser(change.before.id)
+      // check if role update already in Auth
+      if (userRecord.customClaims?.['role'] !== roleAfter) {
+        logger.debug(`[User::onUpdate::RoleChange] id='${change.before.id}' auth='${userRecord.customClaims?.['role']}' db='${roleAfter}', auth update required.`);
+        return await authService.setCustomUserClaims(change.before.id, {role: roleAfter});
+      } else {
+        logger.debug(`[User::onUpdate::RoleChange] id='${change.before.id}' auth='${userRecord.customClaims?.['role']}' db='${roleAfter}', auth update not required.`);
+      }
+      return true
     }
 
     // DisplayName change from Me
