237755522e2cc83590b2960469402af161fd862b
alexcibotari <alexandru.cibotari@gmail.com>
2022-08-25 16:18:32 +0200

    purge cdn cache on publish

--- functions/src/translations.ts
+++ functions/src/translations.ts
@@ -4,60 +4,62 @@ import {BATCH_MAX, bucket, firestoreService, ROLE_ADMIN, ROLE_WRITE} from './con
 import {Space} from './models/space.model';
 import {Translation, TranslationType} from './models/translation.model';
 import {FieldValue, WriteBatch} from 'firebase-admin/firestore';
+import axios from 'axios';
 
 // Publish
 interface PublishTranslationsData {
   spaceId: string
 }
 
-export const publishTranslations = https.onCall((data: PublishTranslationsData, context) => {
+export const publishTranslations = https.onCall(async (data: PublishTranslationsData, context) => {
   logger.info('[publishTranslations] data: ' + JSON.stringify(data));
   logger.info('[publishTranslations] context.auth: ' + JSON.stringify(context.auth));
-  logger.info('[publishTranslations] context.request: ' + JSON.stringify(context.rawRequest));
   if (!SecurityUtils.hasAnyRole([ROLE_WRITE, ROLE_ADMIN], context.auth)) throw new https.HttpsError('permission-denied', 'permission-denied');
   const spaceId: string = data.spaceId;
-  return Promise.all([
-    firestoreService.doc(`spaces/${spaceId}`).get(),
-    firestoreService.collection(`spaces/${spaceId}/translations`).get(),
-  ])
-    .then(([spaceRef, translationsRef]) => {
-      if (spaceRef.exists && !translationsRef.empty) {
-        const space: Space = spaceRef.data() as Space;
-        const translations = translationsRef.docs.filter((it) => it.exists).map((it) => it.data() as Translation);
+  const spaceRef = await firestoreService.doc(`spaces/${spaceId}`).get()
+  const translationsRef = await firestoreService.collection(`spaces/${spaceId}/translations`).get()
+  if (spaceRef.exists && !translationsRef.empty) {
+    const space: Space = spaceRef.data() as Space;
+    const translations = translationsRef.docs.filter((it) => it.exists).map((it) => it.data() as Translation);
 
-        space.locales.forEach((locale) => {
-          const localeJson: { [key: string]: string } = {};
-          translations.forEach((tr) => {
-            let value = tr.locales[locale.id];
-            if (!value) {
-              value = tr.locales[space.localeFallback.id];
-            }
-            localeJson[tr.name] = value;
-          });
-          // Save generated JSON
-          logger.info(`[publishTranslations] Save file to spaces/${spaceId}/translations/${locale.id}.json`);
-          bucket.file(`spaces/${spaceId}/translations/${locale.id}.json`)
-            .save(
-              JSON.stringify(localeJson),
-              (err?: Error | null) => {
-                if (err) {
-                  logger.error(`[publishTranslations] Can not save file for Space(${spaceId}) and Locale(${locale})`);
-                  logger.error(err);
-                }
-              }
-            );
-          // axios.request({
-          //   baseURL: '',
-          //   url: ``,
-          //   method: 'PURGE'
-          // })
-        });
-        return;
-      } else {
-        logger.info(`[publishTranslations] Space ${spaceId} does not exist or no translations.`);
-        throw new https.HttpsError('not-found', 'Space not found');
+    for (const locale of space.locales) {
+      const localeJson: { [key: string]: string } = {};
+      for (const tr of translations) {
+        let value = tr.locales[locale.id];
+        if (!value) {
+          value = tr.locales[space.localeFallback.id];
+        }
+        localeJson[tr.name] = value;
       }
-    });
+      // Save generated JSON
+      logger.info(`[publishTranslations] Save file to spaces/${spaceId}/translations/${locale.id}.json`);
+      bucket.file(`spaces/${spaceId}/translations/${locale.id}.json`)
+        .save(
+          JSON.stringify(localeJson),
+          (err?: Error | null) => {
+            if (err) {
+              logger.error(`[publishTranslations] Can not save file for Space(${spaceId}) and Locale(${locale})`);
+              logger.error(err);
+            }
+          }
+        );
+      const origin = context.rawRequest.header('origin');
+      if (origin && !origin.includes('//localhost:')) {
+        const url  = `/api/v1/spaces/${spaceId}/translations/${locale.id}.json`;
+        await axios.request({
+          baseURL: origin,
+          url: url,
+          method: 'PURGE'
+        })
+        logger.info(`[publishTranslations] purge url ${origin}${url}`);
+      }
+    }
+    return;
+  } else {
+    logger.info(`[publishTranslations] Space ${spaceId} does not exist or no translations.`);
+    throw new https.HttpsError('not-found', 'Space not found');
+  }
+
 });
 
 // Import JSON
