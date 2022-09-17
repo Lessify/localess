82ac36abee0d73141a9a186726178ad99592d728
alexcibotari <alexandru.cibotari@gmail.com>
2022-09-17 22:15:28 +0200

    improve debug

--- functions/src/translate.ts
+++ functions/src/translate.ts
@@ -20,7 +20,7 @@ export const translate = https.onCall(async (data: TranslateData, context) => {
   }
 
   const request: protos.google.cloud.translation.v3.IGetSupportedLanguagesRequest = {
-    parent: `projects/${projectId}/locations/${location}`,
+    parent: `projects/${projectId}/locations/${locationId}`,
     //displayLanguageCode: 'en',
   };
 
