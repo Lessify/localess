11b1ceb64c591a89770989701f7ec13b2482d1bc
Alexandru Cibotari <alexandru.cibotari@gmail.com>
2023-08-25 23:16:14 +0200

    fix download issue

--- src/app/features/tasks/tasks.component.ts
+++ src/app/features/tasks/tasks.component.ts
@@ -82,7 +82,7 @@ export class TasksComponent implements OnInit, OnDestroy {
     this.taskService.downloadUrl(this.selectedSpace!.id, element.id)
       .subscribe({
         next: (url) => {
-          saveAs(url, element.file?.name)
+          saveAs(url, element.file?.name || 'unknown')
         }
       })
     //window.open(`/api/v1/spaces/${this.selectedSpace!.id}/tasks/${element.id}`)
