b16d6fe29756785e49be3cd2c67c32f9c4cfd67c
Alexandru Cibotari <alexandru.cibotari@gmail.com>
2023-08-30 20:12:33 +0200

    add Visual Editor

--- src/scripts/v1.js
+++ src/scripts/v1.js
@@ -8,32 +8,19 @@
   }
 
   if (isInIframe()) {
-    for (const element in document.querySelectorAll('[data-ll-id]')) {
-      element.style.outline = '#003DFF dashed';
-      element.addEventListener('click', (event) => {
-        event.stopPropagation()
-        const id = element.getAttribute('data-ll-id')
-        if (id) {
-          sendEditorData({
-            owner: 'LOCALESS',
-            id: id
-          })
-        }
-      })
-    }
-    // document.querySelectorAll('[data-ll-id]')
-    //   .forEach((element) => {
-    //     element.style.outline = '#003DFF dashed';
-    //     element.addEventListener('click', (event) => {
-    //       event.stopPropagation()
-    //       const id = element.getAttribute('data-ll-id')
-    //       if (id) {
-    //         sendEditorData({
-    //           owner: 'LOCALESS',
-    //           id: id
-    //         })
-    //       }
-    //     })
-    //   });
+    document.querySelectorAll('[data-ll-id]')
+      .forEach((element) => {
+        element.style.outline = '#003DFF dashed';
+        element.addEventListener('click', (event) => {
+          event.stopPropagation()
+          const id = element.getAttribute('data-ll-id')
+          if (id) {
+            sendEditorData({
+              owner: 'LOCALESS',
+              id: id
+            })
+          }
+        })
+      });
   }
 })();
