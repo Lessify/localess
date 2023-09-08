09318e8920fce979bd53c9e9fdd942c64190448e
Alexandru Cibotari <alexandru.cibotari@gmail.com>
2023-09-08 14:10:40 +0200

    update stripe

--- functions/src/plugins/stripe.ts
+++ functions/src/plugins/stripe.ts
@@ -10,12 +10,11 @@ const expressApp = express();
 expressApp
   .use(cors({origin: true}));
 
-expressApp.post('/api/stripe/2023-08-16/spaces/:spaceId/webhook', express.raw({type: '*/*'}), async (req, res) => {
+expressApp.post('/api/stripe/2023-08-16/spaces/:spaceId/webhook', express.raw({type: 'application/json'}), async (req, res) => {
   logger.info('stripe params : ' + JSON.stringify(req.params));
   logger.info('stripe query : ' + JSON.stringify(req.query));
   const sig = req.headers['stripe-signature']!;
   const {spaceId} = req.params;
-  const request = req as any;
   const pluginSnapshot = await findPluginById(spaceId, 'stripe').get();
   if (!pluginSnapshot.exists) {
     res
@@ -37,13 +36,13 @@ expressApp.post('/api/stripe/2023-08-16/spaces/:spaceId/webhook', express.raw({t
   let event: Stripe.Event;
 
   try {
-    event = stripeApp.webhooks.constructEvent(request.rawBody, sig, webhookSigningSecret);
+    event = stripeApp.webhooks.constructEvent((req as any).rawBody, sig, webhookSigningSecret);
   } catch (err: any) {
     res.status(400).send(`Webhook Error: ${err.message}`);
     return;
   }
   logger.info(`event type ${event.type}`);
-  logger.info(JSON.stringify(event));
+  logger.info(JSON.stringify(event.data));
   // Handle the event
   switch (event.type) {
     case 'product.created': {
