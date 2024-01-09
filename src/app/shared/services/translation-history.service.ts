32842ac1cab11d157b9add6cc941458aece0b3fa
alexcibotari <alexandru.cibotari@gmail.com>
2024-01-09 12:25:54 +0100

    Update Translation History

--- src/app/shared/services/translation-history.service.ts
+++ src/app/shared/services/translation-history.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
-import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { collection, collectionData, Firestore, orderBy, query, QueryConstraint } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { traceUntilFirst } from '@angular/fire/performance';
 import { map } from 'rxjs/operators';
 import { TranslationHistory } from '@shared/models/translation-history.model';
+import { limit } from '@firebase/firestore';
 
 @Injectable()
 export class TranslationHistoryService {
   constructor(private readonly firestore: Firestore) {}
 
   findAll(spaceId: string): Observable<TranslationHistory[]> {
-    return collectionData(collection(this.firestore, `spaces/${spaceId}/translations_history`), { idField: 'id' }).pipe(
+    const queryConstrains: QueryConstraint[] = [orderBy('createdAt', 'desc'), limit(20)];
+    return collectionData(query(collection(this.firestore, `spaces/${spaceId}/translations_history`), ...queryConstrains), {
+      idField: 'id',
+    }).pipe(
       traceUntilFirst('Firestore:Translations:findAll'),
       map(it => it as TranslationHistory[])
     );
