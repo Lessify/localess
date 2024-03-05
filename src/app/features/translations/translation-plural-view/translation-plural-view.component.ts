52a5e61c2a289b8164ead8952b08f7bd8482199b
alexcibotari <alexandru.cibotari@gmail.com>
2024-03-05 08:50:09 +0100

    Improve UI

--- src/app/features/translations/translation-plural-view/translation-plural-view.component.ts
+++ src/app/features/translations/translation-plural-view/translation-plural-view.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 interface Plural {
   0: string;
