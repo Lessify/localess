48403ff7f7488717ecf4d8c3fefc9d75e39425a7
Alexandru Cibotari <alexandru.cibotari@gmail.com>
2024-04-22 22:41:00 +0200

    update v1 integration

--- src/scripts/v1.js
+++ src/scripts/v1.js
@@ -9,7 +9,9 @@
 
   if (isInIframe()) {
     document.querySelectorAll('[data-ll-id]').forEach(element => {
-      element.style.outline = '#003DFF dashed';
+      element.style.outline = '#003DFF dashed 1px';
+      element.style.position = 'relative';
+      element.style.display = 'block';
       element.addEventListener('click', event => {
         event.stopPropagation();
         const id = element.getAttribute('data-ll-id');
