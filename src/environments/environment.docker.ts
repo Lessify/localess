9ad175ec176c0131e925dde2dd6b037c5a9ad497
alexcibotari <alexandru.cibotari@gmail.com>
2024-10-02 20:28:25 +0200

    update key

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -2,13 +2,13 @@
 export const environment = {
   appName: 'Localess',
   firebase: {
-    projectId: 'localess-dev',
-    appId: '1:923248451826:web:47779533df41508d8a706b',
-    storageBucket: 'localess-dev.appspot.com',
+    projectId: 'localess-local',
+    appId: '1:19232484518261:web:47779533df41508d8a706b',
+    storageBucket: 'localess-local.appspot.com',
     locationId: 'europe-west6',
     apiKey: 'AIzaSyAGDhKpaTfxnX7kLeXQiuD-1sBWw0z9b2g',
-    authDomain: 'localess-dev.firebaseapp.com',
-    messagingSenderId: '923248451826',
+    authDomain: 'localess-local.firebaseapp.com',
+    messagingSenderId: '19232484518261',
   },
   auth: {
     customDomain: '*',
