02cd1f849ce20ab012914a9a7ab6e9cdb3120c6b
alexcibotari <alexandru.cibotari@gmail.com>
2026-01-01 16:08:54 +0100

    Update UI

--- src/app/features/spaces/schemas/shared/edit-value/edit-value.component.ts
+++ src/app/features/spaces/schemas/shared/edit-value/edit-value.component.ts
@@ -2,17 +2,26 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { FormErrorHandlerService } from '@core/error-handler/form-error-handler.service';
+import { provideIcons } from '@ng-icons/core';
+import { lucideInfo } from '@ng-icons/lucide';
 import { LocalSettingsStore } from '@shared/stores/local-settings.store';
+import { HlmFieldImports } from '@spartan-ng/helm/field';
+import { HlmIconImports } from '@spartan-ng/helm/icon';
+import { HlmInputImports } from '@spartan-ng/helm/input';
+import { HlmTooltipImports } from '@spartan-ng/helm/tooltip';
 
 @Component({
   selector: 'll-schema-value-edit',
   templateUrl: './edit-value.component.html',
   styleUrls: ['./edit-value.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatFormFieldModule, ReactiveFormsModule, MatInputModule, MatExpansionModule, CommonModule],
+  imports: [ReactiveFormsModule, MatExpansionModule, CommonModule, HlmTooltipImports, HlmIconImports, HlmFieldImports, HlmInputImports],
+  providers: [
+    provideIcons({
+      lucideInfo,
+    }),
+  ],
 })
 export class EditValueComponent {
   readonly fe = inject(FormErrorHandlerService);
