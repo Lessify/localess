22b16623ee562c7b9a8de1a7b189314136419001
alexcibotari <alexandru.cibotari@gmail.com>
2026-03-21 21:35:56 +0100

    Update Content Document Status

---
+++ src/app/features/spaces/translations/shared/components/status/status.component.ts
@@ -0,0 +1,53 @@
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { provideIcons } from '@ng-icons/core';
+import { HlmIconImports } from '@spartan-ng/helm/icon';
+import { lucideCircleArrowUp, lucideCircleDotDashed, lucideCircleFadingArrowUp } from '@ng-icons/lucide';
+import { HlmTooltipImports } from '@spartan-ng/helm/tooltip';
+
+@Component({
+  selector: 'll-translation-status',
+  templateUrl: './status.component.html',
+  styleUrls: ['./status.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'flex items-center',
+  },
+  imports: [HlmIconImports, HlmTooltipImports],
+  providers: [
+    provideIcons({
+      lucideCircleDotDashed,
+      lucideCircleArrowUp,
+      lucideCircleFadingArrowUp,
+    }),
+  ],
+})
+export class StatusComponent {
+  updatedAt = input.required<number>();
+  publishedAt = input<number>();
+
+  tooltip = computed(() => {
+    const updatedAt = this.updatedAt();
+    const publishedAt = this.publishedAt();
+    if (publishedAt) {
+      if (publishedAt > updatedAt) {
+        return 'Published';
+      } else if (publishedAt < updatedAt) {
+        return 'Draft';
+      }
+    }
+    return 'Not published';
+  });
+
+  icon = computed(() => {
+    const updatedAt = this.updatedAt();
+    const publishedAt = this.publishedAt();
+    if (publishedAt) {
+      if (publishedAt > updatedAt) {
+        return 'lucideCircleArrowUp';
+      } else if (publishedAt < updatedAt) {
+        return 'lucideCircleFadingArrowUp';
+      }
+    }
+    return 'lucideCircleDotDashed';
+  });
+}
